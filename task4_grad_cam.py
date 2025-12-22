cf91abf207a9e34d762c5f691efae643844564f7
Seyyedhamid Azimidokht <seyedhamidazimidokht@gmail.com>
2025-12-22 18:24:42 +0200

    Feat: Add Grad-CAM code

---
+++ task4_grad_cam.py
@@ -0,0 +1,64 @@
+from utils.train_eval import *
+from utils.losses import *
+
+
+class GradCAM:
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+        self.gradients = None
+        self.activations = None
+        self._register_hooks()
+
+    def _register_hooks(self):
+        def forward_hook(module, input, output):
+            self.activations = output.detach()
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients = grad_output[0].detach()
+
+        self._fwd_handle = self.target_layer.register_forward_hook(forward_hook)
+        self._bwd_handle = self.target_layer.register_full_backward_hook(backward_hook)
+
+    def compute_cam(self, out_size=(IMG_SIZE, IMG_SIZE), eps=1e-8):
+        if self.activations is None or self.gradients is None:
+            raise RuntimeError(
+                "No activations/gradients captured. "
+                "Make sure you ran a forward pass and backward() on a class score "
+                "with gradients enabled before calling compute_cam()."
+            )
+
+        acts = self.activations
+        grads = self.gradients
+
+        weights = grads.mean(dim=(2, 3))
+
+        cam = torch.relu((weights[:, :, None, None] * acts).sum(dim=1))
+
+        cam_min = cam.amin(dim=(1, 2), keepdim=True)
+        cam_max = cam.amax(dim=(1, 2), keepdim=True)
+        cam = (cam - cam_min) / (cam_max - cam_min + eps)
+
+        if out_size is not None:
+            cam = F.interpolate(
+                cam[:, None, :, :],  # [B, 1, H, W]
+                size=out_size,
+                mode="bilinear",
+                align_corners=False,
+            )[:, 0, :, :]
+
+        return cam.detach().cpu().numpy()
+
+    def remove_hooks(self):
+        self._fwd_handle.remove()
+        self._bwd_handle.remove()
+
+
+efficientnet_dir = "pretrained_backbone/ckpt_efficientnet_ep50.pt"
+model = Classifier(backbone="efficientnet", dir=efficientnet_dir).to(device)
+checkpoints_dir = "trained_models/"
+
+print("Efficientnet")
+model.load_state_dict(torch.load(checkpoints_dir + "task1_efficient.pt"))
+cam = GradCAM(model, model.model.features[-1])
+eval_model(model, offsite_test, cam=cam)
